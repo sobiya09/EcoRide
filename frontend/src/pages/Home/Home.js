5dae433464bc5d8272ee22ad255d7bc5345e0fe6
Vithusan19 <vithu0919@gmail.com>
2024-06-06 18:55:40 +0530

    vithu

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -7,6 +7,7 @@ import { Link } from 'react-scroll';
 import insta from '../Assets/insta.png';
 import twi from '../Assets/twitter.png';
 import fb from '../Assets/facebook.png';
+import log_img from '../Assets/log_img.png';
 
 const Home = () => {
   const [overly, setOverly] = useState(false);
@@ -22,19 +23,36 @@ const Home = () => {
   return (
     <>
       {overly && (
-        <div className="layer" onClick={handleClose}>
-          <div className="popup-content" onClick={(e) => e.stopPropagation()}>
-           <p>Vithusan</p>
+        <>
+          <div className="layer" onClick={handleClose}></div>
+          <div className="log-container" onClick={(e) => e.stopPropagation()}>
+            <div className="log-img">
+              <img src={logo} className='log-logo' alt='kjsdfhksj'/>
+            <img src={log_img} alt='logimage' className='loginImage'/>
+            <h4 className='register-tittle'> Welcome, Friend !</h4>
+            <span className='register-text'>Enter your personal details to use all of site features</span>
+            <button className='log-but'>SIGN UP</button>
+            </div>
+            <div className="log-detail">
+              <h1 className="log-title">Login here</h1>
+              <form className="log-form">
+                <input type="text" className="form-control" placeholder="Enter username" />
+                <input type="password" className="form-control" placeholder="Enter password" />
+                <span className='forgot'>Forget Your Password?</span>
+                <button className='login-but'>SIGN IN</button>
+
+              </form>
+            </div>
           </div>
-        </div>
+        </>
       )}
       <div className="navbar-container">
         <img src={logo} alt="logo" className="nav-img" />
         <div className="nav-menu">
           <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500} className="nav-menu-item">Home</Link>
-          <Link activeClass="active" to="about" spy={true} smooth={true} offset={-100} duration={500} className="nav-menu-item">AboutUs</Link>
-          <Link activeClass="active" to="services" spy={true} smooth={true} offset={-100} duration={500} className="nav-menu-item">OurServices</Link>
-          <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-100} duration={500} className="nav-menu-item">ContactUs</Link>
+          <Link activeClass="active" to="about" spy={true} smooth={true} offset={-100} duration={500} className="nav-menu-item">About Us</Link>
+          <Link activeClass="active" to="services" spy={true} smooth={true} offset={-100} duration={500} className="nav-menu-item">Our Services</Link>
+          <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-100} duration={500} className="nav-menu-item">Contact Us</Link>
         </div>
         <button className="nav-button" onClick={handleLogin}>
           Login | Register
@@ -43,13 +61,13 @@ const Home = () => {
       </div>
 
       <section id="about">
-        <h2 className="about-tit">AboutUs</h2>
+        <h2 className="about-tit">About Us</h2>
         <div className="about-container">
           <p className="about-text">
-          Welcome to EcoRide, your go-to platform for simplifying commuting and making it more enjoyable. At EcoRide, we believe that transportation should be convenient, cost-effective, and eco-friendly. That's why we've created a user-friendly web application that connects commuters who share similar routes and schedules, making carpooling easier than ever before.<br/>
-Our mission is simple: to provide a convenient solution for daily travels or occasional trips, while also reducing transportation costs and environmental impact. By connecting users with compatible carpooling partners, we help you save money, reduce your carbon footprint, and enjoy a more social and eco-friendly commuting experience<br/>
-With EcoRide, finding a carpooling partner is effortless. Simply sign up, input your commute details, and let our platform match you with fellow commuters who share your route and schedule. Whether you're looking for a daily ride to work or an occasional trip across town, EcoRide has you covered.<br/>
-Join us in our mission to create a greener, more sustainable future one ride at a time. Together, we can make commuting smarter, more efficient, and better for the planet.
+            Welcome to EcoRide, your go-to platform for simplifying commuting and making it more enjoyable. At EcoRide, we believe that transportation should be convenient, cost-effective, and eco-friendly. That's why we've created a user-friendly web application that connects commuters who share similar routes and schedules, making carpooling easier than ever before.<br />
+            Our mission is simple: to provide a convenient solution for daily travels or occasional trips, while also reducing transportation costs and environmental impact. By connecting users with compatible carpooling partners, we help you save money, reduce your carbon footprint, and enjoy a more social and eco-friendly commuting experience.<br />
+            With EcoRide, finding a carpooling partner is effortless. Simply sign up, input your commute details, and let our platform match you with fellow commuters who share your route and schedule. Whether you're looking for a daily ride to work or an occasional trip across town, EcoRide has you covered.<br />
+            Join us in our mission to create a greener, more sustainable future one ride at a time. Together, we can make commuting smarter, more efficient, and better for the planet.
           </p>
         </div>
         <img src={aboutImg} alt="aboutImage" className="about-img" />
